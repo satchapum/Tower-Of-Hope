3a37ce17a919ad6d00cd73b54c46783d027ed58f
satchapum <satchapum.mail@mail.kmutt.ac.th>
2023-12-05 17:43:10 +0700

    FEAT: Sound Bg and option sound

--- Assets/Scenes/MainMenuScript.cs
+++ Assets/Scenes/MainMenuScript.cs
@@ -5,9 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuScript : MonoBehaviour
 {
+    [SerializeField] GameObject mainmenuCanvas;
+    [SerializeField] GameObject optionCanvas;
     public void WhenButtonStartClick()
     {
         int numberOfFirstScene = 1;
         SceneManager.LoadScene(numberOfFirstScene);
+    }
+    public void WhenOptionButtonClick()
+    {
+        mainmenuCanvas.SetActive(false);
+        optionCanvas.SetActive(true);
+    }
+    
+    public void WhenBackToMainmenuButtonClick()
+    {
+        mainmenuCanvas.SetActive(true);
+        optionCanvas.SetActive(false);
     }
 }
---
+++ Assets/VolumeSetting.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class VolumeSetting : MonoBehaviour
+{
+    [SerializeField] AudioMixer audioMixer;
+    [SerializeField] Slider masterSlider;
+    [SerializeField] Slider musicSlider;
+    [SerializeField] Slider sfxSlider;
+
+    const string MIXER_MASTER = "MasterVolume";
+    const string MIXER_MUSIC = "MusicVolume";
+    const string MIXER_SFX = "SFXVolume";
+
+    private void Awake()
+    {
+        musicSlider.onValueChanged.AddListener(SetMusicVolume);
+        sfxSlider.onValueChanged.AddListener(SetSFXVolume);
+        masterSlider.onValueChanged.AddListener(SetMasterVolume);
+    }
+
+    void SetMusicVolume(float value)
+    {
+        audioMixer.SetFloat(MIXER_MUSIC, Mathf.Log10(value) * 20);
+    }
+
+    void SetSFXVolume(float value)
+    {
+        audioMixer.SetFloat(MIXER_SFX, Mathf.Log10(value) * 20);
+    }
+
+    void SetMasterVolume(float value)
+    {
+        audioMixer.SetFloat(MIXER_MASTER, Mathf.Log10(value) * 20);
+    }
+}
