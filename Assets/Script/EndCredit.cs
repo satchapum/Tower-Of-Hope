104039550b44896ab146adf4c835c0594045f10c
satchapum <satchapum.mail@mail.kmutt.ac.th>
2023-12-09 08:59:32 +0700

    ADD:EndCredit Not Finish

--- Assets/Script/EndCredit.cs
+++ Assets/Script/EndCredit.cs
@@ -1,21 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
-using UnityEngine.SceneManagement;
 
 public class EndCredit : MonoBehaviour
 {
-    [SerializeField] TMP_Text endCreditText;
-    private void Start()
+    [SerializeField] TMP_Text textInstance;
+
+    [SerializeField] float speedPxPerSec;
+
+    public float creditDurationSec;
+    public Rect startRect;
+    public Rect endRect;
+    bool isEnded;
+
+    void Awake()
     {
-        var maxbounds = endCreditText.bounds;
-        Debug.Log(maxbounds);
+        float textHeightPx = LayoutUtility.GetPreferredHeight(this.textInstance.rectTransform);
+        Debug.Log(textHeightPx);
+        this.creditDurationSec = ((textHeightPx + Screen.height) / this.speedPxPerSec) * Time.deltaTime;
+        Debug.Log(creditDurationSec);
     }
 
-    void ReturnToMainMenu()
+    void ResetCreditPosition()
     {
-        GameManager.Instance.ResetData();
-        SceneManager.LoadScene(0);
+        float textHeightPx = LayoutUtility.GetPreferredHeight(this.textInstance.rectTransform);
+        Debug.Log(textHeightPx);
+        this.textInstance.rectTransform.offsetMin = new Vector2(0, -textHeightPx);
+        this.textInstance.rectTransform.offsetMax = new Vector2(0, -Screen.height);
+    }
+
+    void Start()
+    {
+        RestartCredit();
+    }
+
+    void FixedUpdate()
+    {
+        if (!this.isEnded)
+        {
+            this.textInstance.rectTransform.Translate(new Vector2(0, this.speedPxPerSec));
+        }
+    }
+
+    IEnumerator MoveCredit()
+    {
+        this.isEnded = false;
+        yield return new WaitForSeconds(this.creditDurationSec);
+        this.isEnded = true;
+    }
+
+    public void RestartCredit()
+    {
+        this.isEnded = false;
+        ResetCreditPosition();
+        StartCoroutine(MoveCredit());
     }
 }
